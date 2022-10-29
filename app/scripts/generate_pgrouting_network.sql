c65a8fbe7f9f9134675a645d8bd9c0250bfa7307
Aaron Fraint <aaronfraint@icloud.com>
2022-10-28 20:25:36 -0400

    update LTS calc

--- app/scripts/generate_pgrouting_network.sql
+++ app/scripts/generate_pgrouting_network.sql
@@ -4,7 +4,12 @@ create table lts_ways as (
            tonodeno::int as target,
            linklts,
            st_length(geom) * 3.28084 as len_feet,
-           length * (1 + linklts + slopefac) as cost,
+           case
+                when linklts in ('LTS 1', 'Off-road trail/path') then st_length(geom)
+                when linklts = 'LTS 2' then st_length(geom) * 2
+                when linklts = 'LTS 3' then st_length(geom) * 10
+                when linklts = 'LTS 4' then st_length(geom) * 20
+            end as cost,
            10000000000000000000 as reverse_cost,
            st_transform(geom, 4326) as geom
      from
--- app/scripts/generate_pgrouting_network_heavyhanded.sql
+++ app/scripts/generate_pgrouting_network_heavyhanded.sql
@@ -5,10 +5,10 @@ create table lts_ways_heavyhanded as (
            linklts,
            st_length(geom) * 3.28084 as len_feet,
            case
-                when lts = 'LTS 1' then length
-                when lts = 'LTS 2' then length * 10
-                when lts = 'LTS 3' then length * 100
-                when lts = 'LTS 4' then length * 1000
+                when linklts in ('LTS 1', 'Off-road trail/path') then st_length(geom)
+                when linklts = 'LTS 2' then st_length(geom) * 10
+                when linklts = 'LTS 3' then st_length(geom) * 100
+                when linklts = 'LTS 4' then st_length(geom) * 1000
             end as cost,
            10000000000000000000 as reverse_cost,
            st_transform(geom, 4326) as geom
